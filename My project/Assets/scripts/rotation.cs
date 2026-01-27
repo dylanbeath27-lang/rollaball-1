9ac96b1319bdf3b7f63ffde53b6dd099f13ebfba
dylanbeath27-lang <dylan.beath27@tech4maine.org>
2026-01-27 13:33:04 -0500

    new changes
    poggers

--- My project/Assets/scripts/rotation.cs
+++ My project/Assets/scripts/rotation.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
 
- // Update is called once per frame
+ public float speed =1f;
  void Update()
     {
  // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
-        transform.Rotate (new Vector3 (0, 34, 0) * Time.deltaTime);
+        transform.Rotate (new Vector3 (0, 34, 0) * Time.deltaTime * speed);
     }
  
 }
